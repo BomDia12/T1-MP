2f1ef9061bb805890dd6d0e22a0b2261de711cec
BomDia12 <chamatico2@gmail.com>
2021-09-02 16:43:36 -0300

    :white_check_mark: X ou O ganham
    implementando quando o X e quando o O ganham o jogo

--- velha.cpp
+++ velha.cpp
@@ -14,5 +14,87 @@
  */
 
 int VerificaVelha(int velha[3][3]) {
+    int a = velha[0][0];
+    int b = velha[0][1];
+    int c = velha[0][2];
+    int d = velha[1][0];
+    int e = velha[1][1];
+    int f = velha[1][2];
+    int g = velha[2][0];
+    int h = velha[2][1];
+    int i = velha[2][2];
+
+    if (a == b && b == c) {
+        if (a == 1) {
+            return 1;
+        }
+        if (a == 2) {
+            return 2;
+        }
+    }
+
+    if (d == e && e == f) {
+        if (d == 1) {
+            return 1;
+        }
+        if (d == 2) {
+            return 2;
+        }
+    }
+
+    if (g == h && h == i) {
+        if (g == 1) {
+            return 1;
+        }
+        if (g == 2) {
+            return 2;
+        }
+    }
+
+    if (a == d && d == g) {
+        if (a == 1) {
+            return 1;
+        }
+        if (a == 2) {
+            return 2;
+        }
+    }
+
+    if (b == e && e == h) {
+        if (b == 1) {
+            return 1;
+        }
+        if (b == 2) {
+            return 2;
+        }
+    }
+
+    if (c == f && f == i) {
+        if (c == 1) {
+            return 1;
+        }
+        if (c == 2) {
+            return 2;
+        }
+    }
+
+    if (a == e && e == i) {
+        if (a == 1) {
+            return 1;
+        }
+        if (a == 2) {
+            return 2;
+        }
+    }
+
+    if (c == e && e == g) {
+        if (c == 1) {
+            return 1;
+        }
+        if (c == 2) {
+            return 2;
+        }
+    }
+
     return 0; /*!< retorna zero para teste */
 }
