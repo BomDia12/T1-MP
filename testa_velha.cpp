cd1a31e1d8fb43d767fd76eab8541fafe9469dba
BomDia12 <chamatico2@gmail.com>
2021-09-08 16:08:15 -0300

    :test_tube: Teste para ver se e valido
    Adicionados os testes para conferir se a configuraçao do jogo da velha e valido

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -142,3 +142,30 @@ TEST_CASE("Testa se o jogo ainda está indefinido", "[single-file]") {
                         { 0, 0, 0 }};
     REQUIRE(VerificaVelha(teste4) == -1);
 }
+
+TEST_CASE("Testa se o jogo é inválido", "[sigle-file]") {
+    int teste1[3][3] = {{ 1, 0, 0 },
+                        { 0, 0, 1 },
+                        { 1, 1, 0 }};
+    REQUIRE(VerificaVelha(teste1) == -2);
+
+    int teste2[3][3] = {{ 0, 2, 2 },
+                        { 2, 0, 0 },
+                        { 0, 0, 2 }};
+    REQUIRE(VerificaVelha(teste2) == -2);
+
+    int teste3[3][3] = {{ 1, 2, 1 },
+                        { 1, 2, 0 },
+                        { 1, 2, 0 }};
+    REQUIRE(VerificaVelha(teste3) == -2);
+
+    int teste4[3][3] = {{ 1, 2, 0 },
+                        { 0, 0, 1 },
+                        { 1, 1, 0 }};
+    REQUIRE(VerificaVelha(teste4) == -2);
+
+    int teste2[3][3] = {{ 0, 2, 2 },
+                        { 2, 1, 0 },
+                        { 0, 0, 2 }};
+    REQUIRE(VerificaVelha(teste2) == -2);
+}
